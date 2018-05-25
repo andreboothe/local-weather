fd454a0119f0192a982b9e8ff35e29961630b17d
Andre <boothe.andre@live.com>
2018-05-25 08:29:22 -0500

    added styles and background shange functionality

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -6,7 +6,7 @@ const Temperature = ({temp , unit , tempChange}) => {
    
     return (
         <div className=" ">
-            <h1>{temp} <sup>o</sup> {unit.charAt(0)} </h1>
+            <h1 className="temp-display">{temp} <sup>o</sup> {unit.charAt(0)} </h1>
             <button className="btn" onClick={tempChange}>{unit}</button>
         </div>
     );
